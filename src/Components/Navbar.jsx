13eef99e3c65800fcc1e47dc711812890e332e82
SHAJUK10 <shajusrt10.gmail.com>
2023-12-16 20:52:08 +0530

    react-netflix-app

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <img  width={"160px"}   src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+        className='nav_images'
+        alt="" />
+    </div>
+  )
+}
+
+export default Navbar
